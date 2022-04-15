e7d826c69de078bd2750cadd73f1856fe7cea07a
Balder W. Holst <balderwh@gmail.com>
2022-04-15 12:14:09 +0200

    autocommit 2022-04-15

---
+++ .scripts/checks/checks.py
@@ -0,0 +1,29 @@
+import datetime
+import json
+import os
+import subprocess
+
+os.system("source ~/.bash_aliases")
+
+def load_apps():
+    with open('checks.json','r') as f:
+        return(json.load(f))
+
+
+
+apps = load_apps()
+
+date = currentDate = datetime.date.today()
+day = int(currentDate.strftime("%j"))
+
+for app in apps:
+    if (app['last_checked'] + app['frequency'] >= day):
+        cmd = app['cmd'].replace('%d',str(date))
+
+
+        subprocess.run(["notify-send","test",f"Autoscript Running \"{app['name']}\""])
+
+        os.system(cmd)
+
+
+
