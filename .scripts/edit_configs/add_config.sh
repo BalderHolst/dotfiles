b3c1aa1ada98dfd4be989156a0d2c08bc90cfabb
Balder W. Holst <balderwh@gmail.com>
2022-05-02 17:18:19 +0200

    fixed add_config så den tilføjer til dotgit

--- .scripts/edit_configs/add_config.sh
+++ .scripts/edit_configs/add_config.sh
@@ -7,6 +7,6 @@ configs=$(find ~/ -name ".*"; echo "" ; tree -fit ~/.config)
 
 file=$(echo "$configs" | dmenu -i -bw 1 -l 20 -p "add config file:") && name=$(echo "" | dmenu -p "program name:") && \
 	echo "$name - $file" >> $CONFIGS_FILE && \
-	gitconfig add $file && \
+	dotgit add $file && \
 	notify-send "added config" "$name - $file" || \
 	notify-send "configs" "failed to add config"
