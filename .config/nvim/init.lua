e7480a485ec802dcbebe40e392bb22ae836e27e2
Balder W. Holst <balderwh@gmail.com>
2022-07-27 18:01:01 +0200

    auto

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,11 +2,6 @@ require("settings")
 require("mappings")
 require("colors.gruvbox")
 require("plugins")
-require("lsp")
-require("plugins.cmp")
-require("plugins.autopairs")
-require("plugins.flutter-tools").setup()
-require("plugins.luasnip")
 require("myscripts")
 
 
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,59 +1,8 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+require("plugins.packer-list") -- main packer file
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim' -- Packer can manage itself
-
-    -- Dependencies
-    use 'nvim-lua/plenary.nvim' -- Telescope and flutter-tools dependency
-
-    -- Colorschemes
-    use "EdenEast/nightfox.nvim" -- Colorscheme
-    use "ellisonleao/gruvbox.nvim" -- Colorscheme
-
-    -- Misc
-    use 'machakann/vim-highlightedyank' -- Briefly highlight which text was yanked.
-    use 'tpope/vim-commentary' -- comment out lines with gcc and gc
-    use 'junegunn/goyo.vim' -- Center and hide tui
-    use 'windwp/nvim-autopairs' -- Autopairs, integrates with both cmp and treesitter
-
-    -- Telescope
-    use 'nvim-telescope/telescope.nvim' -- main telescope
-    use 'nvim-telescope/telescope-fzf-native.nvim' -- native fuzzy finder for telescope
-
-    -- cmp plugins
-    use "hrsh7th/nvim-cmp" -- The completion plugin
-    use "hrsh7th/cmp-buffer" -- buffer completions
-    use "hrsh7th/cmp-path" -- path completions
-    use "hrsh7th/cmp-cmdline" -- cmdline completions
-    use "saadparwaiz1/cmp_luasnip" -- snippet completion
-    use "hrsh7th/cmp-nvim-lsp" -- LSP
-    use "hrsh7th/cmp-nvim-lua" -- vim type in lua config
-
-    -- More language Plugins
-    use 'dart-lang/dart-vim-plugin' -- Dart
-    use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'} -- Flutter
-    -- use 'thosakwe/vim-flutter' -- Flutter
-    use 'vimwiki/vimwiki'
-
-    -- snippets
-    use "L3MON4D3/LuaSnip" --snippet engine
-    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-
-    -- LSP
-    use "neovim/nvim-lspconfig" -- enable LSP
-    use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-
-    -- Treesitter
-    use {
-	    'nvim-treesitter/nvim-treesitter',
-	    -- run = ':TSUpdate'
-    }
-
-    -- My Plugins
-    -- use "/home/Balder/Projects/nvim_plugins/obsidian_links"
-
-end)
+require("plugins.lsp")
+require("plugins.cmp")
+require("plugins.autopairs")
+require("plugins.flutter-tools").setup()
+require("plugins.luasnip")
 
