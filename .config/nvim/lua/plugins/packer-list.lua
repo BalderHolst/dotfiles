f5fa4ff9e3095e42d656c821dc972ea3a729c71d
Balder W. Holst <balderwh@gmail.com>
2022-08-18 12:18:38 +0200

    Added all nvim configs

---
+++ .config/nvim/lua/plugins/packer-list.lua
@@ -0,0 +1,65 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim' -- Packer can manage itself
+
+	-- Dependencies
+	use 'nvim-lua/plenary.nvim' -- Telescope and flutter-tools dependency
+
+	-- Colorschemes
+	use "EdenEast/nightfox.nvim" -- Colorscheme
+	use "ellisonleao/gruvbox.nvim" -- gruvbox colorscheme
+	use "bluz71/vim-moonfly-colors" -- moonfly colorscheme
+	use "sainnhe/sonokai"
+	use "folke/tokyonight.nvim"
+
+	-- Misc
+	use 'machakann/vim-highlightedyank' -- Briefly highlight which text was yanked.
+	use 'tpope/vim-commentary' -- comment out lines with gcc and gc
+	use 'junegunn/goyo.vim' -- Center and hide tui
+	use 'windwp/nvim-autopairs' -- Autopairs, integrates with both cmp and treesitter
+	use "ahmedkhalf/project.nvim" -- roots nvim in git repos and keeps list of projects (integrates with telescope)
+	-- use 'oberblastmeister/neuron.nvim'
+	-- use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } } -- lua status line
+
+	-- Telescope
+	use 'nvim-telescope/telescope.nvim' -- main telescope
+	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' } -- native fuzzy finder for telescope (better, faster, stronger!)
+	use "nvim-telescope/telescope-file-browser.nvim"
+
+	-- cmp plugins
+	use "hrsh7th/nvim-cmp" -- The completion plugin
+	use "hrsh7th/cmp-buffer" -- buffer completions
+	use "hrsh7th/cmp-path" -- path completions
+	use "hrsh7th/cmp-cmdline" -- cmdline completions
+	use "saadparwaiz1/cmp_luasnip" -- snippet completion
+	use "hrsh7th/cmp-nvim-lsp" -- LSP
+	use "hrsh7th/cmp-nvim-lua" -- vim type in lua config
+
+	-- More language Plugins
+	use 'dart-lang/dart-vim-plugin' -- Dart
+	use { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' } -- Flutter
+	-- use 'thosakwe/vim-flutter' -- Flutter
+
+	-- snippets
+	use "L3MON4D3/LuaSnip" --snippet engine
+	use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+	-- LSP
+	use "neovim/nvim-lspconfig" -- enable LSP
+	use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+	use "jose-elias-alvarez/null-ls.nvim" -- formatter and linter
+
+	-- Treesitter
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		-- run = ':TSUpdate'
+	}
+
+	-- My Plugins
+	-- use "/home/Balder/Projects/nvim_plugins/obsidian_links"
+
+end)
---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,70 @@
+local telescope = require("telescope")
+local fb_actions = require("telescope").extensions.file_browser.actions
+
+local options = {
+	defaults = {
+		prompt_prefix = "  ",
+		selection_caret = "  ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
+			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
+	},
+
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		},
+		file_browser = {
+			theme = "ivy",
+			hijack_netrw = true, -- disables netrw and use telescope-file-browser in its place
+			hidden = true, -- show hidden files
+			hide_parent_dir = true,
+			mappings = {
+				["i"] = { -- your custom insert mode mappings
+					["<C-n>"] = fb_actions.create,
+					["<C-e>"] = fb_actions.create_from_prompt,
+					["<C-r>"] = fb_actions.rename,
+					["<C-d>"] = fb_actions.remove,
+					["<M-h>"] = fb_actions.goto_parent_dir,
+					["<M-l>"] = fb_actions.open,
+					["<C-j>"] = function() fb_actions.move(1) end,
+					-- ["<C-m>"] = fb_actions.move,
+				},
+				["n"] = {
+					["d"] = fb_actions.remove,
+					["l"] = fb_actions.open,
+					["h"] = fb_actions.goto_parent_dir,
+				},
+			},
+		},
+	}
+}
+
+-- setup with options
+require('telescope').setup(options)
+
+-- Extentions
+telescope.load_extension('fzf')
+telescope.load_extension('projects')
+telescope.load_extension('file_browser')
+-- telescope.load_extension('neuron')
+
